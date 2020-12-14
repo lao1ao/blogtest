7e813f013716cd11f2bda882ccd755a6bbbed58b
laobiyo <337198954@qq.com>
2020-12-14 13:33:15 +0800

    6

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   apps : [{
     name: 'app',
-    script: 'www',
-    cwd:"./bin/" ,
+    script: './bin/www',
     // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
     instances: 1,
     autorestart: true,
@@ -30,7 +29,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:laobiyo/blogtest.git',
       path : '/usr/local/myProject',
-      ssh_options:"StrictHostKeyChecking=no",
+      ssh_options:"StrictHostKeyChecking=no"
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       "env":{
         "NODE_ENV":"production"
