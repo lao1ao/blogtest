223fefe2b014c028d89c25fd4feb15660f490498
laobiyo <337198954@qq.com>
2020-12-14 12:44:44 +0800

    4

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'app',
     script: './bin/www',
 
@@ -7,11 +7,11 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: true,
-    ignore_watch: [                           // 不用监听的文件
+    ignore_watch: [ // 不用监听的文件
       "node_modules",
       "logs"
     ],
-    "error_file": "./logs/app-err.log",         // 错误日志文件
+    "error_file": "./logs/app-err.log", // 错误日志文件
     "out_file": "./logs/app-out.log",
     "log_date_format": "YYYY-MM-DD HH:mm:ss", // 给每行日志标记一个时间
     max_memory_restart: '1G',
@@ -23,17 +23,20 @@ module.exports = {
     }
   }],
 
-  deploy : {
-    production : {
-      user : 'root',
-      host : ['121.4.85.149'],
-      ref  : 'origin/master',
-      repo : 'git@github.com:laobiyo/blogtest.git',
-      path : '/usr/local/myProject',
-      ssh_options:"StrictHostKeyChecking=no",
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      "env":{
-        "NODE_ENV":"production"
+  deploy: {
+    production: {
+      user: 'root',
+      host: ['121.4.85.149'],
+      ref: "origin/master",
+      // 远程仓库地址
+      repo: "git@github.com:laobiyo/blogtest.git",
+      // 指定代码拉取到服务器的⽬录
+      path: "/usr/local/myProject",
+      ssh_options: "StrictHostKeyChecking=no",
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env
+      production ',
+      "env": {
+        "NODE_ENV": "production"
       }
     }
   }
