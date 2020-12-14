a6f285aa009126b9d8f3633c731f9de0d0f229dd
laobiyo <337198954@qq.com>
2020-12-14 14:31:09 +0800

    11

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -31,7 +31,10 @@ module.exports = {
       repo : 'git@github.com:laobiyo/blogtest.git',
       path : '/usr/local/myProject',
       ssh_options:"StrictHostKeyChecking=no",
-      'post-deploy' : 'npm install && pm2 reload /source/server/ecosystem.config.js --env production'
+      'post-deploy' : 'npm install',
+      "env":{
+        "NODE_ENV":"production"
+      }
     }
   }
 };
